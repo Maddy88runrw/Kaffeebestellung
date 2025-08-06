3d64cf8a44fe3fbd5263209055eb3c63b4141d0c
Max <maxerfort@googlemail.com>
2025-08-06 16:28:55 +0200

    CORS für alle Domains erlauben

--- bot-server/server.js
+++ bot-server/server.js
@@ -8,11 +8,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: [
-        'http://localhost:5500',
-        'http://127.0.0.1:5500',
-        'https://maddy88runrw.github.io'
-    ],
+    origin: '*',
     credentials: true
 }));
 app.use(express.json());
